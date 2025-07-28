9ebece47093a1fbf2de1c2f18644e24f8fe73006
unknown <yohanesridho70@gmail.com>
2025-07-28 16:53:07 +0700

    prepare deploy to vps

--- main.py
+++ main.py
@@ -460,6 +460,3 @@ def getTask(getid:getID):
         raise HTTPException(status_code=404, detail="ID not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8080)
